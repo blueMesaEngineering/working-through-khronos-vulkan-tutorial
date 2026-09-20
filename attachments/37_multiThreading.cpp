571cb17360e0e3bc2ae75ac9bdcad31c08069af6
blueMesa <nathan@bluemesaengineering.com>
2026-09-20 23:40:25 +0700

    37_multiThreading. Finished mainLoop() edits. WIP.

--- attachments/37_multiThreading.cpp
+++ attachments/37_multiThreading.cpp
@@ -862,15 +862,30 @@ class MultithreadedApplication
 
         void mainLoop()
         {
+		const double			targetFrameTime			= 1.0 / 60.0;
+		
             while (!glfwWindowShouldClose(window))
             {
+		double 				frameStartTime			glfwGetTime();
+		
                 glfwPollEvents();
                 drawFrame();
+		
+		double 				currentTime			= glfwGetTime();
+		lastFrameTime							= (currentTime - lastTime) * 1000.0;
+		lastTime							= currentTime;
+		
+		double				frameTime			= currentTime - frameStartTime;
+		
+		if (frameTime < targetFrameTime)
+		{
+			double			sleepTime			= targetFrameTime - frameTime;
+			std::this_thread::sleep_for(std::chrono::duration<double>(sleepTime));
+		}
             }
 
             device.waitIdle();      // Wait for device to finish operations before destroying resources
         }
-#endif
 
 
 //******************************************************************************************
@@ -3299,6 +3314,8 @@ class MultithreadedApplication
 		
 		return vk::False;
 	}
+        
+
 };
 
 
